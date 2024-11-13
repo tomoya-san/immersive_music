63004cdd0a913097a29581103071d3bc13edd1d6
Tomoya Tanabu <tomoya.tanabu1117@gmail.com>
2024-11-13 13:32:38 +0900

    Completed landmark annotation.

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.GestureRecognition import GestureRecognition
+
+def main():
+    gestureRecognition = GestureRecognition()
+    gestureRecognition.startRecognition()
+
+
+if __name__ == '__main__':
+    main()
